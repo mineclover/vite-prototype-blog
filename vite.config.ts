2dcfe4e5fd9b4db392f398ce8711f94ece3554e6
mineclover <mineclover@naver.com>
2023-05-10 03:23:27 +0900

    env : 종속성 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import remarkRehypePlugin from 'vite-plugin-remark-rehype';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), remarkRehypePlugin()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
