fce2a12640f588e6a2c03b2465039a84a81895b9
mineclover <mineclover@naver.com>
2023-05-14 23:51:09 +0900

    tsx만 업로드

---
+++ src/contents/front/css/border-line/index.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import colors from '@/global/colors';
+import { Jamsil } from '@/global/styleUtils';
+
+const index = () => {
+  return (
+    <BackgroundRoot>
+      <Jamsil>border radius</Jamsil>
+      <BorderRadiusRoot>
+        <BorderRadiusA />
+      </BorderRadiusRoot>
+    </BackgroundRoot>
+  );
+};
+
+export default index;
+
+const BackgroundRoot = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  background: #fff;
+`;
+
+const BorderRadiusRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  background: ${colors.White};
+`;
+const BorderRadiusA = styled.div`
+  width: 64px;
+  height: 320px;
+  background: ${colors.White};
+`;
+const BorderRadiusB = styled.div``;
